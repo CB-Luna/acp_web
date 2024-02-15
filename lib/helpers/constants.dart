58719c159303a57f17f29ce9f37a9f0ab34636a0
AlejandroC <alexcastillo@cbluna.com>
2024-02-15 15:47:33 -0800

    QA Credentials

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,10 +1,10 @@
-//////// DEV CBLUNA ////////
-const String supabaseUrl = 'https://acp.supa.cbluna-dev.com';
+//////// ACP - QA ////////
+const String supabaseUrl = 'https://acpqa.arux.co';
 const String anonKey =
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UiLAogICAgImlhdCI6IDE2NTc2OTU2MDAsCiAgICAiZXhwIjogMTgxNTQ2MjAwMAp9.8h6s6K2rRn20SOc7robvygAWNhZsSWD4xFRdIZMyYVI';
-final redirectUrl = '${Uri.base.origin}/cambio-contrasena';
+const redirectUrl = '$supabaseUrl/change-pass/#/change-password/token';
 const themeId = String.fromEnvironment('themeId', defaultValue: '2');
 
-String apiGatewayUrl = 'https://acp.supa.cbluna-dev.com/acp/api';
+String apiGatewayUrl = '$supabaseUrl/acp/api';
 
 const int mobileSize = 800;
